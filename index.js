8d55c465b1ca1dec80b86af28459f725ec921764
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-19 18:16:38 +0700

    feat: add function stocks can return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.

--- index.js
+++ index.js
@@ -85,3 +85,29 @@ const pascalTriangel = (numRows) => {
 }
 
 pascalTriangel(5).forEach((data) => console.log(data));
+
+console.log("___________________________________________________________");
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+const maxProfit = (prices) => {
+    if (prices.length < 2) return 0;
+
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[j] - prices[i] > maxProfit) {
+                maxProfit = prices[j] - prices[i];
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+// Test cases
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
